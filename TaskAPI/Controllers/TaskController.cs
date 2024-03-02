8c8a01085c3486461fba2dae46a02eeccdac59d1
Mical_SSD <Mical_SSD@DESKTOP-JJIU4T4>
2024-03-02 23:51:33 +0530

    Add project files.

---
+++ TaskAPI/Controllers/TaskController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Task()
+        {
+            var tasks = new string[] { "Task 1", "Task 2", "Task 3" };
+            return Ok(tasks);
+        }
+    }
+}
