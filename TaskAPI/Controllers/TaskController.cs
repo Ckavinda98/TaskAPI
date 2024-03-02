b804490823a3e5843aa7c79fd5cef4a356b6d3f2
Charuka Kavinda <ckavinda57@gmail.com>
2024-03-03 00:10:54 +0530

    Adding new Task

--- TaskAPI/Controllers/TaskController.cs
+++ TaskAPI/Controllers/TaskController.cs
@@ -11,7 +11,7 @@ namespace TaskAPI.Controllers
         [HttpGet]
         public IActionResult Task()
         {
-            var tasks = new string[] { "Task 1", "Task 2", "Task 3" };
+            var tasks = new string[] { "Task 1", "Task 2", "Task 3", "Task 4" };
             return Ok(tasks);
         }
 
