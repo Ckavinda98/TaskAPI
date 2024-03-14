4e629fac8a3791c3b2f46bec1ed53fb106fc26b9
Charuka Kavinda <ckavinda57@gmail.com>
2024-03-14 14:45:41 +0530

    adding connection to the db

---
+++ TaskApi.DataAccess/TodoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskAPI.Models;
+
+namespace TaskApi.DataAccess
+{
+    public class TodoDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=DESKTOP-JJIU4T4; Database=MyToDoDB; TrustServerCertificate=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
